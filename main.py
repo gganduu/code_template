6f09671c8e404d715a12542891016636198ea5a3
gganduu_zz <gganduu_zz@163.com>
2021-03-28 18:11:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import utils.common as common
 import argparse
-import torch
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -8,14 +7,12 @@ def get_parser():
     parser.add_argument('-i', '--ipex', action='store_true', help='this is to specify use IPEX or not')
     parser.add_argument('-t', '--train', action='store_true', help='training or inference')
     parser.add_argument('-d', '--data',  help='input data, could be a image or a image path')
-    parser.add_argument('-g', '--gpu',  action='store_true', help='gpu or cpu')
     return parser
 
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
     config = common.parse_yaml(args.config)
-    device = torch.device('cuda') if args.gpu else torch.device('cpu')
     type = config.get('model').get('type')
     if type == 'classification':
         model = __import__('core.cls.classification', fromlist=['classification'])
@@ -31,6 +28,6 @@ if __name__ == "__main__":
             model.test(config, args.data, ipex.DEVICE)
     else:
         if args.train:
-            model.train(config, device)
+            model.train(config, 'cpu')
         else:
-            model.test(config, args.data, device)
+            model.test(config, args.data, 'cpu')
